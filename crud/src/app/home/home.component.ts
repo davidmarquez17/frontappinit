588655b71da14af10caa765f5dbdd3842485545f
davidmarquez17 <david.marquez@uptc.edu.co>
2023-06-20 12:03:22 -0500

    'clientesempresas'

--- crud/src/app/home/home.component.ts
+++ crud/src/app/home/home.component.ts
@@ -7,20 +7,111 @@ import { CrudService } from '../service/crud.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  data: any[] = [];
-
+  clientes: any[] = [];
+  empresas: any[] = [];
+  nombreEmpresa:any;
+  direccionEmpresa:any;
+  idEmpresa:any;
+  nombreCliente:any;
+  apellidoCliente:any;
+  clienteId:any;
+  empresaCliente: any;
   constructor(private crudService: CrudService){
 
   }
 
-  ngOnInit(): void {
-    //this.obtenerUsuarios();
+  async ngOnInit(): Promise<any> {
+    await this.obtenerEmpresas();
+    this.obtenerClientes();    
   }
 
-  obtenerUsuarios(){
-    this.crudService.getData().subscribe( data => {
-      this.data = data;
-      console.log(this.data);
+  async obtenerClientes(){
+    this.crudService.getClientes().subscribe( async data => {
+      var clientes = await this.asignarNombreEmpresa(data);
+      this.clientes = clientes;
     })
+  }
+
+  agregarCliente(){
+    let newCliente = {
+      "nombre": this.nombreCliente,
+      "apellido": this.apellidoCliente,
+      "empresa": this.empresas.find(emp => emp.nombre === this.empresaCliente).idEmpresa
+    }
+    this.crudService.postCliente(newCliente).subscribe( res => {
+      this.obtenerClientes();
+    })
+  }
+
+  editarCliente(){
+    let editCliente = {
+      "nombre": this.nombreCliente,
+      "apellido": this.apellidoCliente,
+      "empresa":this.empresas.find(emp => emp.nombre === this.empresaCliente).idEmpresa,
+      "clienteId": this.clienteId
+    }
+    this.crudService.editCliente(editCliente).subscribe( res => {
+      this.obtenerClientes();
+    })
+  }
+
+  eliminarCliente(id:number){
+    this.crudService.deleteCliente(id).subscribe( res => {
+      this.obtenerClientes();
+    })
+  }
+
+  clienteAEditar(nombre:string, apellido:string, id:number, empresa:number){
+    this.nombreCliente = nombre;
+    this.apellidoCliente = apellido;
+    this.clienteId = id;
+    this.empresaCliente = empresa;
+  }
+
+  async obtenerEmpresas(){
+    this.crudService.getEmpresas().subscribe( async data => {      
+      this.empresas = data;
+    })
+  }
+
+  agregarEmpresa(){
+    let newEmpresa = {
+      "nombre": this.nombreEmpresa,
+      "direccion": this.direccionEmpresa
+    }
+    this.crudService.postEmpresa(newEmpresa).subscribe( res => {
+      this.obtenerEmpresas();
+    })
+  }
+
+  editarEmpresa(){
+    let editEmpresa = {
+      "nombre": this.nombreEmpresa,
+      "direccion": this.direccionEmpresa,
+      "idEmpresa": this.idEmpresa
+    }
+    this.crudService.editEmpresa(editEmpresa).subscribe( res => {
+      this.obtenerEmpresas();
+    })
+  }
+
+  eliminarEmpresa(id:number){
+    this.crudService.deleteEmpresa(id).subscribe( res => {
+      this.obtenerEmpresas();
+    })
+  }
+
+  empresaAEditar(nombre:string, direccion:string, id:number){
+    this.nombreEmpresa = nombre;
+    this.direccionEmpresa = direccion;
+    this.idEmpresa = id;    
+  }
+
+  async asignarNombreEmpresa(data:any[]){
+    let clientes = data;
+    for (let i = 0; i < clientes.length; i++) {
+      clientes[i].nombreEmpresa = this.empresas.find(emp => emp.idEmpresa === data[i].empresa).nombre;
+    }
+    return clientes;
   }
 }
--- crud/src/app/service/crud.service.ts
+++ crud/src/app/service/crud.service.ts
@@ -7,11 +7,41 @@ import { Observable } from 'rxjs';
 })
 export class CrudService {
 
-  private urlApi = 'https://localhost:7083/Usuarios';
+  private urlApiClientes = 'https://localhost:7083/Clientes';
+
+  private urlApiEmpresas = 'https://localhost:7083/Empresas';
 
   constructor(private http: HttpClient) { }
 
-  public getData(): Observable<any>{
-    return this.http.get<any>(this.urlApi);
+  public getClientes(): Observable<any>{
+    return this.http.get<any>(this.urlApiClientes);
+  }
+
+  public postCliente(cliente:any): Observable<any>{
+    return this.http.post<any>(this.urlApiClientes + '/Create', cliente);
+  }
+
+  public editCliente(cliente:any): Observable<any>{
+    return this.http.put<any>(this.urlApiClientes + '/Edit/' + cliente.clienteId, cliente);
+  }
+
+  public deleteCliente(id:number): Observable<any>{
+    return this.http.delete<any>(this.urlApiClientes + '/Delete/' + id);
+  }
+
+  public getEmpresas(): Observable<any>{
+    return this.http.get<any>(this.urlApiEmpresas);
+  }
+
+  public postEmpresa(empresa:any): Observable<any>{
+    return this.http.post<any>(this.urlApiEmpresas + '/Create', empresa);
+  }
+
+  public editEmpresa(empresa:any): Observable<any>{
+    return this.http.put<any>(this.urlApiEmpresas + '/Edit/' + empresa.idEmpresa, empresa);
+  }
+
+  public deleteEmpresa(id:number): Observable<any>{
+    return this.http.delete<any>(this.urlApiEmpresas + '/Delete/' + id);
   }
 }
